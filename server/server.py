a13bda08f690269195b6ba3b7f395f100c9c3e4a
James Partin <jamesaustinpartin@gmail.com>
2023-07-14 16:13:09 -0400

    initial configuration implementation

--- server/server.py
+++ server/server.py
@@ -1,8 +1,38 @@
-from flask import Flask, jsonify, request
+import configparser
 import subprocess
+from flask import Flask, jsonify, request, Response
+from configparser import ConfigParser
+config = ConfigParser()
 
-app = Flask(__name__)
 
+app = Flask(__name__, instance_relative_config = True)
+
+while True:
+    try:
+        #TODO: Replace printouts with a proper logging solution
+        app.config.from_pyfile('configuration.ini')
+        break
+
+    except FileNotFoundError:
+        print("File does not exist, creating...")
+        with open('/'.join([app.instance_path, 'configuration.ini']), 'x'):
+            pass
+        print("File successfully created!")
+
+
+@app.route('/api/set-temp-file-destination', methods=['POST'])
+def handle_filepath():
+    if request.method == 'POST' and 'path' in request.form:
+        filepath = str(request.form['path'])
+        value = app.config.get('CONFIG_KEY')
+        return Response(status= 200)
+    else:
+        print("Invalid request recieved")
+        return Response(status= 400)
+    
+    
+
+    
 
 @app.route('/api/upload', methods=['POST'])
 def handle_upload():
