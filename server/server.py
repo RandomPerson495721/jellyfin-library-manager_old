0d161184b97503827c4400a1faa8109ec5bda013
James Partin <jamesaustinpartin@gmail.com>
2023-07-19 11:19:27 -0400

    finished set-temp-file destination request

--- server/server.py
+++ server/server.py
@@ -1,46 +1,72 @@
 import configparser
+import os
 import subprocess
-from flask import Flask, jsonify, request, Response
+
+from flask import Flask , jsonify , request , Response , redirect
 from configparser import ConfigParser
-config = ConfigParser()
 
+config: ConfigParser = ConfigParser()
 
-app = Flask(__name__, instance_relative_config = True)
+app: Flask = Flask(__name__ , instance_relative_config=True)
+
+ConfigFile: str = '/'.join([app.instance_path , 'configuration.ini'])
+
+IsNewConfig: bool = False
 
 while True:
-    try:
-        #TODO: Replace printouts with a proper logging solution
-        app.config.from_pyfile('configuration.ini')
+    if os.path.exists('/'.join([app.instance_path , 'configuration.ini'])):
+        # TODO: Replace printouts with a proper logging solution
+        config.read('configuration.ini', '')
         break
 
-    except FileNotFoundError:
+    else:
         print("File does not exist, creating...")
-        with open('/'.join([app.instance_path, 'configuration.ini']), 'x'):
+        with open('/'.join([app.instance_path , 'configuration.ini']) , 'x'):
             pass
+        IsNewConfig = True
         print("File successfully created!")
 
 
-@app.route('/api/set-temp-file-destination', methods=['POST'])
+def write_configs(file: str):
+    with open(file, 'w') as conf:
+        config.write(conf)
+
+
+def set_default_configs():
+    # Add default configs
+    config['REQUIRED'] = dict(TempFilePath='/'.join([app.instance_path, 'tmp/']))
+    write_configs(ConfigFile)
+
+
+if IsNewConfig:
+    set_default_configs()
+
+
+@app.route('/api/set-temp-file-destination' , methods=['POST'])
 def handle_filepath():
-    if request.method == 'POST' and 'path' in request.form:
-        filepath = str(request.form['path'])
-        value = app.config.get('CONFIG_KEY')
-        return Response(status= 200)
-    else:
-        print("Invalid request recieved")
-        return Response(status= 400)
-    
-    
+    filepath = str(request.form['path'])
 
-    
+    if filepath.lower().startswith('users') and not filepath.lower().startswith('/users'):
+        filepath = '/' + filepath
 
-@app.route('/api/upload', methods=['POST'])
+    while True:
+        if request.method == 'POST' and 'path' in request.form and os.path.exists(filepath):
+            config['REQUIRED'] = dict(TempFilePath=filepath)
+            write_configs(ConfigFile)
+            return Response(status=200)
+        elif request.method == 'POST' and 'path' in request.form and not os.path.exists(filepath):
+            os.makedirs(filepath)
+        else:
+            print("Invalid request recieved")
+            return Response(status=400)
+
+
+@app.route('/api/upload' , methods=['POST'])
 def handle_upload():
+    # TODO: Implement file upload logic
 
-    # Implement file upload logic here
     return jsonify({'message': 'Upload endpoint'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
