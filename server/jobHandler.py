91c4dc0a2b95430b9003b10fa1a817d0343b6ebf
James Partin <jamesaustinpartin@gmail.com>
2023-07-31 00:19:29 -0400

    added error handling for upload

--- server/jobHandler.py
+++ server/jobHandler.py
@@ -1,7 +1,8 @@
 import configparser
 import os
 import traceback
-from flask import Flask , jsonify , request , make_response
+import threading
+from flask import Flask, jsonify, request, make_response
 
 
 class jobHandler:
@@ -11,13 +12,39 @@ class jobHandler:
     file: request.stream
 
     # TODO: Implement metadata
-    def __init__(self , filename: str , filesize: int , file: request.stream,  chunksize: int = 4096):
+    def __init__(self, filename: str, filesize: int, file: request.stream, chunksize: int = 4096):
+        self.lock = threading.Lock()
         self.filename = filename
         self.chunksize = chunksize
         self.filesize = filesize
         self.file = file
 
     def start_upload(self):
+        try:
+            with open(self.filename, "wb") as output:
+                while True:
+                    buffer = self.file.read(self.chunksize)
+                    if len(buffer) > 0:
+                        output.write(buffer)
+                    else:
+                        break
+            return make_response("Upload Successful!", 200)
+        except IOError:
+            return make_response("Error occurred when writing file!", 500)
+        except Exception:
+            return make_response("Unknown server error occurred!", 500)
 
+    def get_progress(self):
+        filesize: int
+        status_code: int
+        try:
+            filesize = os.path.getsize(self.filename)
+            status_code = 200
+        except Exception:
+            filesize = -1
+            status_code = 500
 
-
+        return make_response(jsonify({
+            'uploaded_file_size': filesize,
+            'other_progress_indicators': 'need_to_be_implemented'}),
+            status_code)
